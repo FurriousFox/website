f4340a1d065471aae76d4bd16b474623a2b83bbe
Micha <micha@argv.nl>
2025-10-05 16:53:20 +0200

    fonts are hard

--- script.js
+++ script.js
@@ -7,5 +7,4 @@ setTimeout(() => document.getElementById("argv").innerText += "v", 1000 + 900);
 setTimeout(() => {
     const cursor = document.getElementsByClassName("cursor")[0];
     cursor.style = "animation: blink 2s steps(1) infinite";
-
 }, 1000 + 900 + 250);
