38a3f3516d2250e7327a1708712c10d461d784a7
Micha <micha@argv.nl>
2025-10-06 14:33:09 +0200

    add server compatible with my weirdly complex nginx config

---
+++ server.ts
@@ -0,0 +1,38 @@
+import http from "node:http";
+import path from "node:path";
+import fs from "node:fs";
+import mime from "mime-types";
+import process from "node:process";
+
+const base_dir = import.meta.dirname;
+if (base_dir === undefined) throw new Error("dirname is undefined");
+
+process.on("uncaughtException", console.log);
+process.on("unhandledRejection", console.log);
+
+const server = http.createServer((req, res) => {
+    if (req.method !== "GET" ||
+        (req.headers["host"] !== "site.argv.nl" &&
+            req.headers["host"] !== "jsite.argv.nl" &&
+            req.headers["host"] !== "argv.nl" &&
+            req.headers["host"] !== "www.argv.nl")
+    ) { res.writeHead(404); res.end(); return; }
+
+    if (req.url == undefined) req.url = "/";
+    if (req.url == "/") req.url = "/index.html";
+
+    const req_url = new URL(req.url, "http://localhost");
+    const req_path = path.join(base_dir, req_url.pathname);
+    const relative_path = path.relative(base_dir, req_path);
+    if (relative_path.startsWith("..")) { res.writeHead(404); res.end(); return; }
+
+    if (!fs.existsSync(req_path) ||
+        !(fs.statSync(req_path).isFile())) { res.writeHead(404); res.end(); return; }
+
+    res.writeHead(200, "OK", {
+        "content-type": `${mime.lookup(req_path) || 'application/octet-stream'}`,
+    });
+    fs.createReadStream(req_path).pipe(res);
+    return;
+});
+server.listen(5113, "127.0.0.1");
